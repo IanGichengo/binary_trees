50e8198ff55bbb7aee718db45e9da170f3bb1147
IanGichengo <iangatimu6@gmail.com>
2024-03-29 16:15:59 -0400

    binary_trees

--- binary_trees.h
+++ binary_trees.h
@@ -68,5 +68,7 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second);
 
 #endif
